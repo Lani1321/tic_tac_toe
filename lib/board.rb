292230bc03d5304c7dbd8e716c897c2de4b4b03b
Lani1321 <lani.berry13@gmail.com>
2017-09-29 11:58:44 -0400

    add #initialize and #display to create and display ttt board

---
+++ lib/board.rb
@@ -0,0 +1,17 @@
+class Board
+  attr_accessor :cells
+
+  def initialize           
+    @cells = Array.new(9, " ")
+  end
+
+  def display
+    puts "\n"      
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  end
+
+end
