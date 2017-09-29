d67377b51ce4ff96c9bf68defabd5cebaa96751e
Lani1321 <lani.berry13@gmail.com>
2017-09-29 11:59:22 -0400

    add tests for board model

---
+++ spec/board_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require_relative '../lib/board'
+
+RSpec.describe "TicTacToeTest", :type => :request do
+  describe "Board" do
+    let(:board){Board.new}
+
+    describe '#initialize' do
+      it 'sets the cells of the board to a 9 element array of " "' do
+        board = Board.new
+        expect(board.cells).to match_array(Array.new(9, " "))
+      end
+    end
+
+    describe '#cells' do
+      it 'has an attribute cells' do
+        expect(board).to respond_to(:cells)
+
+        board.cells = []
+        expect(board.cells).to match_array([])
+      end
+    end
+  end
+
+end
