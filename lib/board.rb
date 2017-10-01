490b5771d0e55e4145d256e561057fb73823d3dd
Lani1321 <lani.berry13@gmail.com>
2017-10-01 00:28:31 -0400

    change board to 3 arrays

--- lib/board.rb
+++ lib/board.rb
@@ -2,19 +2,27 @@ class Board
   attr_accessor :cells
 
   def initialize           
-    @cells = Array.new(9, " ")
+    @cells = Array.new(3) { Array.new(3) {" "}}
   end
 
   def display
     puts "\n"      
-    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts " #{cells[0][0]} | #{cells[0][1]} | #{cells[0][2]} "
     puts "-----------"
-    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts " #{cells[1][0]} | #{cells[1][1]} | #{cells[1][2]} "
     puts "-----------"
-    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    puts " #{cells[2][0]} | #{cells[2][1]} | #{cells[2][2]} "
   end
 
   def position(input)
-    @cells[input.to_i - 1]
+    if input == "1" || "2" || "3"
+      @cells[0][input.to_i - 1]
+    elsif input == "4" || "5" || "6"
+      @cells[1][input.to_i - 1]
+    else
+      @cells[2][input.to_i - 1]
+    end
   end
+
+  
 end
