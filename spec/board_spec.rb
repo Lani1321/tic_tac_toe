c99a44c4822b336c45fc6cef8adac4dac237cb96
Lani1321 <lani.berry13@gmail.com>
2017-10-01 00:31:53 -0400

    update board test with correct board array

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "TicTacToe-BoardTest", :type => :request do
     describe '#initialize' do
       it 'sets the cells of the board to a 9 element array of " "' do
         board = Board.new
-        expect(board.cells).to match_array(Array.new(9, " "))
+        expect(board.cells).to match_array(Array.new(3) { Array.new(3) {" "}})
       end
     end
 
