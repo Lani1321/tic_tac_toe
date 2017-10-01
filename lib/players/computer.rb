3ab86f4a662e8b2147eb0985a1d094eb8405dd72
Lani1321 <lani.berry13@gmail.com>
2017-10-01 01:53:16 -0400

    computer move

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,13 @@
 module Players
   class Computer < Player
     def move(board)
+      
+      # Always go for the middle spot if it's available
+      if board.cells[1][1] == " "
+        move = "5"
+      else
+        move = Random.rand(9).to_s
+      end
     end
   end
 end
